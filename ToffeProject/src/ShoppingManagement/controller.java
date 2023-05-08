dc53463d24eff93096e687417bb34b81c845aca5
Sohaila Abdelazim Khalifa <sohailakhalifa03@gmail.com>
2023-05-08 21:45:11 +0300

    class update

--- ToffeProject/src/ShoppingManagement/controller.java
+++ ToffeProject/src/ShoppingManagement/controller.java
@@ -1,4 +1,12 @@
 package ShoppingManagement;
 
 public class controller {
+    private map <Integer,order> orders ;
+    private map <String,product> products ;
+
+    public void addItem(){};
+    public void placeOrder(){};
+    public void updateQuantity(){};
+    public void updatePQtyInStock(){};
+    public void paymentType(){};
 }
--- ToffeProject/src/ShoppingManagement/payment.java
+++ ToffeProject/src/ShoppingManagement/payment.java
@@ -1,4 +1,12 @@
 package ShoppingManagement;
 
-public class payment {
+public abstract class payment {
+    private double amount ;
+    public payment(double amount ){
+        this.amount = amount ;
+    }
+    public double getAmount () {
+        return amount;
+    }
+
 }
